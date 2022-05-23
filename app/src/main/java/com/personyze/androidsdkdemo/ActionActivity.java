9b3d447213b083c042412be6f60a0268ef898d5e
Jeremiah Shaulov <jeremiah.shaulov@gmail.com>
2022-05-23 06:56:16 +0300

    To Android Studio 2021.2.1

--- app/src/main/java/com/personyze/androidsdkdemo/ActionActivity.java
+++ app/src/main/java/com/personyze/androidsdkdemo/ActionActivity.java
@@ -3,7 +3,7 @@ package com.personyze.androidsdkdemo;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.webkit.WebView;
--- app/src/main/java/com/personyze/androidsdkdemo/MainActivity.java
+++ app/src/main/java/com/personyze/androidsdkdemo/MainActivity.java
@@ -4,39 +4,39 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 
 /// This is first screen that prompts to enter API key
 public class MainActivity extends AppCompatActivity
 {	/// "extra" name for intent (see below)
-	public static final String EXTRA_API_KEY = "com.personyze.androidsdkdemo.API_KEY";
+    public static final String EXTRA_API_KEY = "com.personyze.androidsdkdemo.API_KEY";
 
-	/// I will store API that you enter in text field to storage and reload it on next run.
-	protected SharedPreferences storage;
+    /// I will store API that you enter in text field to storage and reload it on next run.
+    protected SharedPreferences storage;
 
-	/// This method is called by Android, when he starts my application.
-	@Override protected void onCreate(Bundle savedInstanceState)
-	{	super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    /// This method is called by Android, when he starts my application.
+    @Override protected void onCreate(Bundle savedInstanceState)
+    {	super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-		// Reload API key from storage
-		storage = getSharedPreferences("Personyze Demo", Context.MODE_PRIVATE);
-		String apiKey = storage.getString("API Key", "");
-		((EditText)findViewById(R.id.editKey)).setText(apiKey);
-	}
+        // Reload API key from storage
+        storage = getSharedPreferences("Personyze Demo", Context.MODE_PRIVATE);
+        String apiKey = storage.getString("API Key", "");
+        ((EditText)findViewById(R.id.editKey)).setText(apiKey);
+    }
 
-	/// You entered API key. We will move on to the next screen
-	public void clickedOk(View view)
-	{	String apiKey = ((EditText)findViewById(R.id.editKey)).getText().toString();
-		// Store key
-		SharedPreferences.Editor editor = storage.edit();
-		editor.putString("API Key", apiKey);
-		editor.apply();
-		// Go to next activity
-		Intent intent = new Intent(this, DemoActivity.class);
-		intent.putExtra(EXTRA_API_KEY, apiKey);
-		startActivity(intent);
-	}
+    /// You entered API key. We will move on to the next screen
+    public void clickedOk(View view)
+    {	String apiKey = ((EditText)findViewById(R.id.editKey)).getText().toString();
+        // Store key
+        SharedPreferences.Editor editor = storage.edit();
+        editor.putString("API Key", apiKey);
+        editor.apply();
+        // Go to next activity
+        Intent intent = new Intent(this, DemoActivity.class);
+        intent.putExtra(EXTRA_API_KEY, apiKey);
+        startActivity(intent);
+    }
 }
